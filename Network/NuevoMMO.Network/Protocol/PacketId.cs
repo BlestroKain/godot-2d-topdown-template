ab7d0b33e554b872da4abf1409a380d86241b6e9
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2026-09-09 22:31:37 -0500

    Register account protocol messages

--- Network/NuevoMMO.Network/Protocol/PacketId.cs
+++ Network/NuevoMMO.Network/Protocol/PacketId.cs
@@ -4,7 +4,9 @@ public enum PacketId : ushort
 {
     ConnectRequest = 1, LoginRequest = 2, CharacterListRequest = 3, CreateCharacterRequest = 4,
     CharacterSelectRequest = 5, MapReadyRequest = 6, MoveRequest = 7, Ping = 8, DisconnectRequest = 9,
+    RegisterRequest = 10,
     ConnectionAccepted = 100, LoginResult = 101, CharacterListResult = 102, CharacterCreated = 103,
     CharacterSelected = 104, MapLoad = 105, SpawnEntity = 106, DespawnEntity = 107,
-    EntityMoved = 108, EntityState = 109, ServerTime = 110, Pong = 111, Error = 112
+    EntityMoved = 108, EntityState = 109, ServerTime = 110, Pong = 111, Error = 112,
+    RegisterResult = 113
 }
