8055e3e6ff06403ac3666bc165923fc76fd2b732
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2026-09-09 23:42:30 -0500

    Upgrade EnvironmentEditor for map environment settings

--- Editor/NuevoMMO.Editor/Maps/EnvironmentEditor.cs
+++ Editor/NuevoMMO.Editor/Maps/EnvironmentEditor.cs
@@ -1,3 +1,17 @@
+using NuevoMMO.Core;
+
 namespace NuevoMMO.Editor;
 
-public sealed class EnvironmentEditor { }
+public sealed class EnvironmentEditor
+{
+    private MapDocument? document;
+
+    public void Bind(MapDocument map) => document = map ?? throw new ArgumentNullException(nameof(map));
+
+    public MapEnvironmentDefinition Get() => RequireDocument().Environment;
+
+    public void Set(MapEnvironmentDefinition environment)
+        => RequireDocument().Environment = environment ?? throw new ArgumentNullException(nameof(environment));
+
+    private MapDocument RequireDocument() => document ?? throw new InvalidOperationException("No hay mapa abierto.");
+}
