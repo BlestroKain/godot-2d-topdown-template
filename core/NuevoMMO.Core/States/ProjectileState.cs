8c43107c45e7774b9ae682a31d0b5b80ff6e401d
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2026-09-11 07:19:59 -0500

    Remove duplicate lowercase Core tree

--- core/NuevoMMO.Core/States/ProjectileState.cs
+++
@@ -1,5 +0,0 @@
-namespace NuevoMMO.Core;
-
-public sealed record ProjectileState(EntityId Id, DefinitionId? Definition, MapInstanceId MapInstance,
-    Vector2Data Position, Vector2Data Velocity, Direction Facing, ContentKey VisualKey, string DisplayName)
-    : EntityState(Id, EntityKind.Projectile, Definition, MapInstance, Position, Velocity, Facing, VisualKey, DisplayName);
--- core/NuevoMMO.Core/Stats/StatBlock.cs
+++
@@ -1,8 +0,0 @@
-namespace NuevoMMO.Core;
-
-public sealed class StatBlock
-{
-    public PrimaryStats Primary { get; } = new();
-    public SecondaryStats Secondary { get; } = new();
-    public ResistanceStats Resistances { get; } = new();
-}
