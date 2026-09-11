e6d56b4bfbc58518678e9d6c50f03c4752806ac3
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2026-09-11 14:28:08 -0500

    Require Tradition when creating characters

--- Network/NuevoMMO.Network/Packets/CreateCharacterRequest.cs
+++ Network/NuevoMMO.Network/Packets/CreateCharacterRequest.cs
@@ -2,10 +2,16 @@ using NuevoMMO.Core;
 
 namespace NuevoMMO.Network;
 
-public sealed class CreateCharacterRequest(SessionId session, string sessionToken, string name) : IPacket
+public sealed class CreateCharacterRequest(
+    SessionId session,
+    string sessionToken,
+    string name,
+    DefinitionId traditionId) : IPacket
 {
     public SessionId Session { get; } = session;
     public string SessionToken { get; } = sessionToken;
     public string Name { get; } = name;
-    public override string ToString() => $"CreateCharacterRequest {{ Session = {Session}, SessionToken = [REDACTED], Name = {Name} }}";
+    public DefinitionId TraditionId { get; } = traditionId;
+    public override string ToString() =>
+        $"CreateCharacterRequest {{ Session = {Session}, SessionToken = [REDACTED], Name = {Name}, TraditionId = {TraditionId} }}";
 }
